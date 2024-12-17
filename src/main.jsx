d6a316f24dad0750f5cd76a2cdb8e8da3b447227
AYUSH SINGH <ayush@AYUSHs-Air.lan>
2024-12-17 10:23:14 +0530

    init

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Home from './components/Home.jsx'
+import Landing from './components/Landing.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Landing />} />
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
